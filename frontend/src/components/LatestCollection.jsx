2e9434d7fffde35754322c05215c3d514810b41f
Bo2874 <ngovanbo280704@gmail.com>
2025-03-14 01:34:39 +0700

    third-commit

--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -2,7 +2,7 @@ import React, { useContext, useEffect, useState } from 'react'
 import { ShopContext } from '../context/ShopContext'
 import Title from './Title';
 import { products } from '../assets/assets';
-import ProductItem from './Productitem';
+import ProductItem from './ProductItem';
 
 const LatestCollection = () => {
 
